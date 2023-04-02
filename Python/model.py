fc913438e15d98b0c7cdec2bd60080f3e1aa1154
Treyvin Daniels <62913102+kingtrey32@users.noreply.github.com>
2023-04-02 10:53:59 -0400

    Add files via upload
    adding the .py files to the python folder

---
+++ Python/model.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 28 15:11:45 2023
+
+@author: Ender
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+#*************************************************The CNN model***********************
+#We will need to change a few things about it to fit her criteria like leakyReLu etc.
+
+class Fashion_Class_Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+        #
+        self.leaky_relu = nn.LeakyReLU(negative_slope=0.1)
+
+    def forward(self, x):   
+        x = self.conv1(x)
+        x = F.leaky_relu(x)
+        x = self.pool(x)
+        #x = self.pool(F.relu(self.conv1(x)))
+        
+        x = self.conv2(x)
+        x = F.leaky_relu(x)
+        x = self.pool(x)
+        #x = self.pool(F.relu(self.conv2(x)))
+        
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        
+        x = self.fc1(x)
+        x = F.leaky_relu(x)
+        #x = F.relu(self.fc1(x))
+        
+        x = self.fc2(x)
+        x = F.leaky_relu(x)
+        #x = F.relu(self.fc2(x))
+
+        x = self.fc3(x)
+        return x
+
+#*************************************************************************************
+
+
+
+#provides a label, or Class, to the object in question
+#not used here
+def output_label(label) :
+    output_mapping = {
+        0: "T-shirt/Top",
+        1: "Trouser",
+        2: "Pullover",
+        3: "Dress",
+        4: "Coat",
+        5: "Sandal",
+        6: "Shirt",
+        7: "Sneaker",
+        8: "Bag",
+        9: "Ankle Boot" }
+    
+    input = (label.item() if type(label) == torch.Tensor else label)
+    return output_mapping[input]
